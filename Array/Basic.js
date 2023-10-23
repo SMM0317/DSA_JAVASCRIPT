40a8421af4fbc71c81ab180a3d964e37733bcaa7
swapnil manale <swapnilmanale9922@gmail.com>
2023-10-23 22:48:41 +0530

    ADD: find the nth fibonacci number

--- Array/Basic.js
+++ Array/Basic.js
@@ -47,3 +47,23 @@ copyNum = Math.floor(copyNum/10)
 }   
  return x === reverseNum
 };
+
+// 5. find the nth fibonacci number
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1.
+let fib = function(n) {
+    if(n < 2){
+        return n;
+    }
+
+    let prev = 0, curr = 1, next;
+    for(let i=2; i<= n; i++){
+        next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return next;
+};
+
+// Fibonacci Sequence: 0 1 1 2 3 5 8...
+console.log(fib(5)); // 5
+console.log(fib(10)); // 55
