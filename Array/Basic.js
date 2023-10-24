e51f2cff5a12cc3909de9a7d532dc03819b5ffb2
swapnil manale <swapnilmanale9922@gmail.com>
2023-10-24 21:36:33 +0530

    ADD: Missing number in an array

--- Array/Basic.js
+++ Array/Basic.js
@@ -67,3 +67,18 @@ let fib = function(n) {
 // Fibonacci Sequence: 0 1 1 2 3 5 8...
 console.log(fib(5)); // 5
 console.log(fib(10)); // 55
+
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+let missingNumber = function(nums) {
+    let sum = 0;
+    for(let i=0; i<nums.length; i++){
+        sum += nums[i];
+    }
+    return nums.length*(nums.length+1)/2 - sum;
+};
+
+// One Line Solution: 
+// let missingNumber = (nums) => nums.length*(nums.length+1)/2 - nums.reduce((acc, num) => num + acc);
+
+console.log(missingNumber([3,0,1])); // 2
+console.log(missingNumber([9,6,4,2,3,5,7,0,1])); // 8
